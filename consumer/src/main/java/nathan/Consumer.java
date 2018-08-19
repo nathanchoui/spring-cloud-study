aba48d46928fa5ac807e90f92f999035565015bd
nathan <nathan_choui@163.com>
2018-08-19 23:27:43 +0800

    add hystrix dashboard

--- consumer/src/main/java/nathan/Consumer.java
+++ consumer/src/main/java/nathan/Consumer.java
@@ -5,11 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
 @EnableFeignClients
 @EnableCircuitBreaker
 @EnableDiscoveryClient
+@EnableHystrixDashboard
 public class Consumer {
 
 	public static void main(String[] args) {
