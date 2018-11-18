e3d09a3a1ea5acf8ac21a06c975e3b14bb817c56
nathan <nathan_choui@163.com>
2018-11-18 16:57:53 +0800

    update to spring cloud f

--- demoService/src/main/java/personal.nathan/DemoServiceApp.java
+++ demoService/src/main/java/personal.nathan/DemoServiceApp.java
@@ -1,16 +1,19 @@
-package nathan;
+package personal.nathan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+/**
+ * Description:
+ * <p>
+ * Created by nathan.z on 18-11-18.
+ */
 @SpringBootApplication
-@EnableCircuitBreaker
 @EnableDiscoveryClient
-public class Consumer {
+public class DemoServiceApp {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Consumer.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoServiceApp.class, args);
+    }
 }
